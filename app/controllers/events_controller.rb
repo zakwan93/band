ad6cf0137ef323740521817756fa09609cfb6837
zakwan93 <zakwan_bhaiyat@yahoo.com>
2018-01-04 23:50:55 -0500

    Created all add form using Bootstrap

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,17 @@ class EventsController < ApplicationController
   		@events = Event.all
 	end
 
+
+	def new
+		@events = Event.new
+	end
+
+	def show
+		@events = Event.find(params[:id])
+	end
+
+	def create
+		@Events= Event.create(band_id: params[:band_id],venue_id: params[:venue_id],event_date: params[:event_date], alcohol_served: params[:alcohol_served])
+		redirect_to events_path
+	end
 end

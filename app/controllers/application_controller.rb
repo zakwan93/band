b66cb0f926164f6344b66f38216eec0e798e1c25
zakwan93 <zakwan_bhaiyat@yahoo.com>
2018-01-07 20:46:46 -0500

    work on update query for all pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+  def index
+  		@venues = Venue.all
+  		@bands = Band.all
+  		@events = Event.all
+	end
+
 end
